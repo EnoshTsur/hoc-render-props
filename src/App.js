14bf3b4f34544efd88fa524f66dd3621a30adb35
gilos <gil@sasover.com>
2020-06-16 20:10:15 +0300

    render-props

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import React from 'react';
-import H1View from './components/H1View/H1View'
-import toggle from './components/Toggle/Toggle'
+import React from  'react'
 
-const AView = ({ isToggleOn, handleToggle }) => (
-    <div>
-        <a href="#">{`${ isToggleOn}`}</a>
-        <button onClick={handleToggle}>Click</button>
-    </div>
-
-)
-
-const PView = ({ isToggleOn, handleToggle, }) => (
-    <div>
-        <p>{`${isToggleOn}`}</p>
-        <button onClick={handleToggle}>Click ME!</button>
-    </div>
-)
-
-const WithH1 = toggle(H1View)
-const WithA = toggle(AView)
-const WithP = toggle(PView)
 function App() {
 
   return (
     <div>
-        <WithH1 />
-        <WithA />
-        <WithP />
+       
    </div>
   );
 }
